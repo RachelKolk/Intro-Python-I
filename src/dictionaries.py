17337f62e3d720e8cabb9aa4fddb9c47e2e1522d
RachelKolk <rach.m.obrien@gmail.com>
2019-04-30 15:08:09 -0400

    Dome with the first three directories for today - comprehensions, dictionaries, & functions

--- src/dictionaries.py
+++ src/dictionaries.py
@@ -35,10 +35,21 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+dict = {"lat": 42,"lon": -120, "name": "my fave place"}
+waypoints.append(dict)
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # YOUR CODE HERE
+for x in waypoints:
+    if x['name'] == "a place":
+        x['name'] = "not a real place"
+        x['lon'] = -130
+
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for x in waypoints: 
+    print(x['lat'] and x['lon'])
